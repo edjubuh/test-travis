08d9a69999ad0c9f1951c4974257c6b3c8b74633
Elliot Berman <edjubuh@gmail.com>
2017-07-06 18:59:34 -0500

    Use dynamic bintray.json

--- bintray.py
+++ bintray.py
@@ -9,12 +9,11 @@ o = {
         },
         'version': {
             'name': open('version').read().strip(),
-            'desc': str(subprocess.check_output(['git', 'log', '-1', '--format=%B'])).strip(),
-            'vcs_tag': open('version').read().strip(),
+            'desc': str(subprocess.check_output(['git', 'log', '-1', '--format=%B'])).strip()
         },
         'files': [
-            { 'includePattern': './(pros_cli-.*\\\\.zip)', 'uploadPattern': '$1' },
-            { 'includePattern': './dist/(.*\\\\.whl)', 'uploadPattern': '$1' }
+            { 'includePattern': './(pros_cli-.*\\.zip)', 'uploadPattern': '$1' },
+            { 'includePattern': './dist/(.*\\.whl)', 'uploadPattern': '$1' }
         ]
     }
 
